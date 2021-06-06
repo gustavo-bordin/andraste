859da337b91868453e303d508181dc9f652d888a
Gustavo Bordin <self.bordin@gmail.com>
2021-06-06 03:24:23 -0300

    adjusted test

--- tests/args_test.py
+++ tests/args_test.py
@@ -1,7 +1,5 @@
 import unittest
-import sys
 
-sys.path.append("..")
 from andraste import _create_parser
 from andraste import _create_args
 
@@ -14,10 +12,11 @@ VALID_ARGS = [
 class TestArgs(unittest.TestCase):
     def test_should_create_args(self):
         parser = _create_parser()
-        args = _create_args(parser)
-        
+        parser_with_args = _create_args(parser)
+        parsed_args = parser_with_args.parse_args(['bot.bot.MySpider'])
+
         for arg in VALID_ARGS:
-            self.assertIn(arg, vars(args))
+            self.assertIn(arg, vars(parsed_args))
         
 if __name__ == '__main__':
     unittest.main()

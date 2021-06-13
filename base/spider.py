36119006897e23bfa07fd83d667e3310acba40b3
Gustavo Bordin <self.bordin@gmail.com>
2021-06-13 04:15:00 -0300

    removed provisory things

--- base/spider.py
+++ base/spider.py
@@ -1,11 +1,7 @@
 from scrapy import Spider
 
 class Andraste(Spider):
-    @classmethod
-    def from_crawler(cls, crawler, *args, **kwargs):
-        spider = super(Andraste, cls).from_crawler(crawler, *args, **kwargs)
-        signals.spider_closed = handle_spider_closed()
-        return spider
+    pass
 
     
 

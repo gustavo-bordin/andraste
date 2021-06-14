4e8a52eb26e9c8665e17161987a84562989542f2
Gustavo Bordin <self.bordin@gmail.com>
2021-06-13 22:51:58 -0300

    sleeping between each process

--- core/flow.py
+++ core/flow.py
@@ -1,3 +1,6 @@
+import json
+import time
+
 from scrapy.crawler import CrawlerProcess
 from crochet import setup
 
@@ -14,13 +17,18 @@ class Flow:
         input_received = args[3]
         print(f'Received: {input_received}')
 
-        runner = CrawlerProcess()
-        runner.crawl(self.crawler_class, input=input_received)
+        json_input = json.loads(input_received)
 
+        runner = CrawlerProcess()
+        runner.crawl(self.crawler_class, input_=json_input)
+        time.sleep(1)
         print('Input processed, getting next input...')
 
     def start(self):
-        queue_name = get_queue_name(self.crawler_class)
+        queue_name = get_queue_name(
+            class_name=self.crawler_class.__name__,
+            queue_type="inputs"
+        )
         queue = Queue(queue_name)
         queue.setup()
         
